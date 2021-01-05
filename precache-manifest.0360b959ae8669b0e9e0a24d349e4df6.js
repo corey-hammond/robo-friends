03d6d078582353da860089faff476cc8d61b43cd
Corey Hammond <coreyjhammond@gmail.com>
2021-01-05 10:49:33 -0800

    Updates

---
+++ precache-manifest.0360b959ae8669b0e9e0a24d349e4df6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca11dc34cb09f63d958d08b928f87682",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "5629e29903776bee648f",
+    "url": "/robo-friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cb8328ba1adc5ee21055",
+    "url": "/robo-friends/static/css/main.c676c760.chunk.css"
+  },
+  {
+    "revision": "5629e29903776bee648f",
+    "url": "/robo-friends/static/js/2.a9c43ba2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends/static/js/2.a9c43ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8328ba1adc5ee21055",
+    "url": "/robo-friends/static/js/main.1edb6a01.chunk.js"
+  },
+  {
+    "revision": "1fc1d57fba5bc44bf7e3",
+    "url": "/robo-friends/static/js/runtime-main.efa5b401.js"
+  }
+]);
